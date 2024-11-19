5903c21588333bc8e20fbd2789e8bfd568b92504
Brock Altug <brock.altug99@gmail.com>
2024-11-18 17:05:02 -0800

    Fix User model to align with 'users' table and implement secure password hashing

--- models/User.js
+++ models/User.js
@@ -57,7 +57,7 @@ User.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'users', // Explicitly matches 'users' table
   }
 );
 
